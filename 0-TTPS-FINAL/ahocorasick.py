7db365409f740fba4d74304ef480bbd241357561
Facundo Gazar <74885262+FacundoGazar@users.noreply.github.com>
2026-03-06 16:26:29 -0300

    aho corasick

---
+++ 0-TTPS-FINAL/ahocorasick.py
@@ -0,0 +1,74 @@
+from collections import deque
+
+class Node:
+    def __init__(self):
+        self.outputs = set()
+        self.children = {}
+        self.failure_link = None
+
+    def has_child(self, key):
+        return key in self.children
+
+    def get_child(self, key):
+        return self.children.get(key)
+
+    def set_child(self, key, node):
+        self.children[key] = node
+
+    def add_output(self, output):
+        self.outputs.add(output)
+
+    def copy_outputs(self, node):
+        for o in node.outputs:
+            self.outputs.add(o)
+
+
+class AhoCorasick:
+    def __init__(self, patterns):
+        self.root = Node()
+
+        for pattern in patterns:
+            curr_node = self.root
+            for key in pattern:
+                if not curr_node.has_child(key):
+                    curr_node.set_child(key, Node())
+                curr_node = curr_node.get_child(key)
+            curr_node.add_output(pattern)
+
+        self.root.failure_link = self.root
+        queue = deque()
+        
+        for key, child in list(self.root.children.items()):
+            child.failure_link = self.root
+            queue.append((key, child))
+
+        while queue:
+            _, curr_node = queue.popleft()
+
+            for key, child in list(curr_node.children.items()):
+                failure = curr_node.failure_link
+                child.failure_link = failure.get_child(key) or self.root
+
+                child.copy_outputs(child.failure_link)
+
+                queue.append((key, child))
+
+            failure = curr_node.failure_link
+            for key in list(failure.children.keys()):
+                if not curr_node.has_child(key):
+                    curr_node.set_child(key, failure.get_child(key))
+
+    def search(self, text):
+        found = []
+        state = self.root
+        i = 0
+        while i < len(text):
+            c = text[i]
+            if state.has_child(c):
+                state = state.get_child(c)
+                i += 1
+                for val in state.outputs:
+                    found.append({"pos": i - len(val), "val": val})
+            else:
+                i += 1
+        return found
